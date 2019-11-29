96acdfa84ceb2752f415bc4e06b5dc6bebdfa3a8
irisrucia97 <57894962+irisrucia97@users.noreply.github.com>
2019-11-29 17:14:26 +0900

    11/29 로그인 구현

--- php/paichai/logincheck.php
+++ php/paichai/logincheck.php
@@ -2,17 +2,18 @@
 session_start();
 $id=$_POST['id'];
 $pw=$_POST['pw'];
-$db=mysqli_connect("localhost","root","irisrucia97","user_info");
+$db=mysqli_connect("localhost","root","irisrucia97","kidrian");
  
 $check="SELECT * FROM user_info WHERE userid='$id'";
 $result=mysqli_query($db,$check); 
-if($result==1){
-    $row=$result->fetch_array(MYSQLI_ASSOC); //하나의 열을 배열로 가져오기
+if($result){
+    $row=mysqli_fetch_array($result); //하나의 열을 배열로 가져오기
+	echo $row['userid'];
     if($row['userpw']==$pw){  //MYSQLI_ASSOC 필드명으로 첨자 가능
         $_SESSION['userid']=$id;           //로그인 성공 시 세션 변수 만들기
         if(isset($_SESSION['userid']))    //세션 변수가 참일 때
         {
-            header('Location: ./logintest.php');   //로그인 성공 시 페이지 이동
+            header('Location: logintest.php');   //로그인 성공 시 페이지 이동
         }
         else{
             echo "세션 저장 실패";
@@ -23,6 +24,6 @@ if($result==1){
     }
 }
 else{
-    echo "wrong id or pw";
+    echo "db연결 실패";
 }
 ?>
--- php/paichai/signup.php
+++ php/paichai/signup.php
@@ -4,11 +4,10 @@ $pw=$_POST['pw'];
 $pwc=$_POST['pwc'];
 $name=$_POST['name'];
 $email=$_POST['email'];
-$result = "a";
 if($pw!=$pwc) //비밀번호와 비밀번호 확인 문자열이 맞지 않을 경우
 {
     echo "비밀번호와 비밀번호 확인이 서로 다릅니다.";
-    echo "<a href=signUp.html>back page</a>";
+    echo "<a href=signup.html>back page</a>";
     exit();
 }
 if($id==NULL || $pw==NULL || $name==NULL || $email==NULL) //빈칸 체크
